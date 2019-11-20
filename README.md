8c1c3fdc29bab6cb7573a052209d457bee76cfab
Aliaksei <lehaSVV2009@gmail.com>
2019-11-20 21:47:00 +0300

    Add lambda to post weather to telegram

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # weather-to-telegram-lambda
+
+[AWS Lambda](https://aws.amazon.com/lambda/) to send [weather](https://wttr.in) to [Telegram](https://telegram.org/) channel via [bot](https://tele.gs/botfather)
+
+## Getting Started
+
+Create `.env`
+
+```
+CITY=Moscow
+TELEGRAM_CHAT_ID=...
+TELEGRAM_BOT_TOKEN=...
+```
+
+Install
+
+```
+$ yarn
+$ yarn test
+```
+
+## Authors
+
+- [**Alex Soroka**](https://lehasvv2009.github.io/resume/)
---
+++ index.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+
+const axios = require("axios");
+const FormData = require("form-data");
+const fs = require("fs");
+
+exports.handler = async event => {
+  console.log(event);
+
+  try {
+    const TMP_FILE_PATH = "/tmp/weather.png";
+
+    const wttrResponse = await axios.get(
+      `https://wttr.in/${process.env.CITY || "Minsk"}.png?1&m`,
+      {
+        responseType: "stream"
+      }
+    );
+    wttrResponse.data.pipe(fs.createWriteStream(TMP_FILE_PATH));
+
+    const formData = new FormData();
+    formData.append("chat_id", process.env.TELEGRAM_CHAT_ID);
+    formData.append("photo", fs.createReadStream(TMP_FILE_PATH));
+    const formHeaders = formData.getHeaders();
+    await axios.post(
+      `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendPhoto`,
+      formData,
+      {
+        headers: { ...formHeaders }
+      }
+    );
+
+    console.log("Success");
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Success" })
+    };
+  } catch (e) {
+    console.log("Error", e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: e.message })
+    };
+  }
+};
